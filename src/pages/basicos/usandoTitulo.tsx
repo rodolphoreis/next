71b959ddedafb51bb2f8cf87c55dc080c5a06992
Rodolpho Reis <rodolphoreis@live.com>
2024-05-27 20:52:14 +0100

    feat: create page

---
+++ src/pages/basicos/usandoTitulo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Title from "../../../components/Title";
+
+const usandoTitulo = () => {
+  return (
+    <div>
+      <h2>Usando Título</h2>
+      <Title principal="Rodolpho" secundario="Reis" />
+    </div>
+  );
+};
+
+export default usandoTitulo;
