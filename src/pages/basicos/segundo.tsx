c5c25516ce9ba927716e2423a64ae1992559c7ca
Rodolpho Reis <rodolphoreis@live.com>
2024-05-27 16:40:38 +0100

    feat: add form

--- src/pages/basicos/segundo.tsx
+++ src/pages/basicos/segundo.tsx
@@ -1,5 +1,29 @@
+import { type } from "os";
+import { useState } from "react";
+
 const segundo = () => {
-  return <h1>Segundo</h1>;
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nome
+          <input
+            type="text"
+            value={name}
+            name="name"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </label>
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
 };
 
 export default segundo;
