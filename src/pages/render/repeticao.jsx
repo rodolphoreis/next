5852f1b0f9e310d2966825f7e0a49699ed222009
Rodolpho Reis <rodolphoreis@live.com>
2024-05-31 16:47:37 +0100

    feat: add table and render list

--- src/pages/render/repeticao.jsx
+++ src/pages/render/repeticao.jsx
@@ -1,7 +1,31 @@
+import { listenerCount } from "process";
+import listaProdutos from "../../../data/listaProdutos";
+
 const repeticao = () => {
+  const renderizarLinhas = () => {
+    return listaProdutos.map((produto) => {
+      return (
+        <tr key={produto.id}>
+          <td>{produto.id}</td>
+          <td>{produto.nome}</td>
+          <td>{produto.preco}</td>
+        </tr>
+      );
+    });
+  };
+
   return (
     <div>
-      <h2>repeticao</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Código</th>
+            <th>Nome</th>
+            <th>Preço</th>
+          </tr>
+        </thead>
+        <tbody>{renderizarLinhas()}</tbody>
+      </table>
     </div>
   );
 };
