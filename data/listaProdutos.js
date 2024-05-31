b72f0bf2a697a60adae816b09fc55bb5688c3c0d
Rodolpho Reis <rodolphoreis@live.com>
2024-05-31 16:21:59 +0100

    feat: create list

---
+++ data/listaProdutos.js
@@ -0,0 +1,9 @@
+import Produto from "../model/produto";
+
+export default [
+  new Produto(1, "iPad", 3450),
+  new Produto(2, "iphone", 1250),
+  new Produto(3, "Node24", 1100),
+  new Produto(4, "Monitor 36", 900),
+  new Produto(5, "Mac Mini", 5600),
+];
