25ea19d4aa84f1038c2660c61931cf77091826c7
Rodolpho Reis <rodolphoreis@live.com>
2024-06-01 18:59:26 +0100

    feat: use component

--- src/pages/render/condicional.jsx
+++ src/pages/render/condicional.jsx
@@ -1,7 +1,18 @@
+import SomentePar from "../../../components/SomentePar";
+
 const condicional = () => {
   return (
     <div>
-      <h2>condicional</h2>
+      <h3>Somente Par</h3>
+
+      <SomentePar numero={1} />
+      <SomentePar numero={2} />
+      <SomentePar numero={3} />
+      <SomentePar numero={4} />
+      <SomentePar numero={5} />
+      <SomentePar numero={6} />
+      <SomentePar numero={7} />
+      <SomentePar numero={8} />
     </div>
   );
 };
