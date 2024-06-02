fce3fa2e8921d7cd0a04fb35d97787fc383284bc
Rodolpho Reis <rodolphoreis@live.com>
2024-06-02 09:04:55 +0100

    feat: add style

--- components/Lista.jsx
+++ components/Lista.jsx
@@ -2,7 +2,7 @@ const Lista = (props) => {
   return (
     <div>
       <h1>Lista de Algo</h1>
-      <ul> {props.children}</ul>
+      <ul style={{ listStyle: "none" }}>{props.children}</ul>
     </div>
   );
 };
