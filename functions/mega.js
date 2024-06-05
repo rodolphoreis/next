52c04c941688057fcf6b9eb476f8034984b48a00
Rodolpho Reis <rodolphoreis@live.com>
2024-06-05 20:36:52 +0100

    feat: add sort

--- functions/mega.js
+++ functions/mega.js
@@ -1,14 +1,16 @@
-export function mega(quantidade = 6, numeros = []) {
+function mega(quantidade = 6, numeros = []) {
   if (quantidade < 6 && quantidade > 60) {
     throw "Quantidade invalida!";
   }
   if (numeros.length === quantidade) {
-    return numeros;
+    return numeros.sort((n1, n2) => n1 - n2);
   }
 
   const numeroAleatorio = parseInt(Math.random() * 60) + 1;
 
   if (!numeros.includes(numeroAleatorio)) {
-    return mega(quantidade, [...numeros]);
+    return mega(quantidade, [...numeros, numeroAleatorio]);
+  } else {
+    return mega(quantidade, numeros);
   }
 }
