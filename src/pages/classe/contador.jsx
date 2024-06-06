fe037c6eacfdcf02e63bf1ad719916077dcc3a4f
Rodolpho Reis <rodolphoreis@live.com>
2024-06-06 15:42:17 +0100

    feat: create component

---
+++ src/pages/classe/contador.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import Contador from "../../../components/Contador";
+
+export default class ContadorPage extends Component {
+  render() {
+    <div>
+      <Contador />
+      <h2>Reis</h2>
+    </div>;
+  }
+}
