6fb04c7c066187f2d8a8b4575748d0b4e318e197
Rodolpho Reis <rodolphoreis@live.com>
2024-06-06 15:46:28 +0100

    feat: add state

--- components/Contador.jsx
+++ components/Contador.jsx
@@ -1,6 +1,10 @@
 import { Component } from "react";
 
 export default class Contador extends Component {
+  state = {
+    numero: 7,
+  };
+
   render() {
     <h1>Componente feito com class</h1>;
   }
