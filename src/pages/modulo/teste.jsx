c3386d3bbb1b10e888450aef01e469becce4be00
Rodolpho Reis <rodolphoreis@live.com>
2024-06-07 17:18:41 +0100

    feat: add component

--- src/pages/modulo/teste.jsx
+++ src/pages/modulo/teste.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Comp1 from "../../../components/funcionais";
 
 const teste = () => {
   return (
     <div>
       <h2>Teste</h2>
+
+      <Comp1 />
     </div>
   );
 };
